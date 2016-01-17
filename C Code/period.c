ac43f3be86c8a3ff95333696bf08a8c2e3a775da
Marco Grillo <mark0s_01@hotmail.com>
2016-01-17 16:44:16 +0100

    Update period.c

--- C Code/period.c
+++ C Code/period.c
@@ -1,27 +1,3 @@
-//////////////////////////////////////////////////////////////////
-//                          SEMANTICA                           //
-//////////////////////////////////////////////////////////////////
-/* Sia x una stringa non vuota.  Un intero p tale che
-0 < p ≤|x| si dice essere "un periodo di x" se
-
-  x[i] = x[i + p]
-
-per i = 0, 1, ... , |x| − p − 1.
-Si noti che, per ogni stringa non vuota, la lunghezza della stringa
-è un periodo della medesima.  In tal modo, ogni stringa non vuota
-ha almeno un periodo.  E' quindi ben definito il concetto di minimo
-periodo di una stringa x, denotato da per(x):
-
-  per(x) = min { p | p è un periodo di x }.
-
-Scriva una funzione C
-
-   unsigned per(const char x[], unsigned l)
-
-che data una stringa x di lunghezza l restituisce per(x). */
-//////////////////////////////////////////////////////////////////
-
-
 #include <stdio.h>
 #include <string.h>
 
