c64680797228f9a1f33278f3bdb5c3d711e40545
MarcoGrillo <mark0s_01@hotmail.com>
2016-01-18 11:48:49 +0100

    updates on ACSL

--- C Code/period.c
+++ C Code/period.c
@@ -29,30 +29,47 @@ che data una stringa x di lunghezza l restituisce per(x). */
 /*@ 
     requires l >= 0;
     requires \valid(x+(0..(l-1)));
+    requires p >= 0;
 
+    behavior zero:
+        assumes p == 0;
+        ensures \result = 1;
+
+    behavior one: 
+        assumes p > 0;
+        ensures \forall int i; 0 <= i < l-p-1; x[i] == x[i+p]
+
+    complete behaviors one,zero;
+    disjoint behaviors one,zero;
 */
 
 unsigned has_period(char x[], int p, unsigned l) {
     if (p == l) return 1;
     if ((l % p) != 0) return 0;
 
-        for (int i = 0 ; i < l - p - 1 ; ++i) {
-            if (x[i] != x[i + p])
-                return 0;
+        /*@
+            loop assigns i;
+        */
+        for (int i = 0 ; i < l-p-1 ; ++i) {
+            if (x[i] == x[i + p])
+                return 1;
         }
 
-    return 1;
+    return 0;
 }
 
 /*@
     requires l >= 0;
-    requires \valid(x+(0..(l-1)));
-
+    requires \valid(x+(0..l-1));
+    
     ensures 0 < \result <= l;
 */
 
 unsigned per(char x[], unsigned l) {
     int p = 1;
+    /*@
+        loop assigns p;
+    */
     while (p <= l && !has_period(x, p, l))
         ++p;
     return p;
