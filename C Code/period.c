0d48c1ef89cbf026cdf7ec731ba26dc875a1731c
MarcoGrillo <mark0s_01@hotmail.com>
2016-01-19 18:31:46 +0100

    fixed C code and ACSL code

--- C Code/period.c
+++ C Code/period.c
@@ -21,71 +21,69 @@ Write a C function
 that, given a string x of length l, returns per(x). */
 //////////////////////////////////////////////////////////////////
 
+
 /*@ 
     requires l >= 0;
-    requires \valid(x+(0..(l-1)));
     requires p >= 0;
-
-    behavior zero:
-        assumes p == 0;
-        ensures \result = 1;
-
+    
+    //behavior zero:
+      //assumes \forall int i; 0 <= i < l-p-1 ==> p == 1;
+      //ensures \result == 1;
+    
     behavior one: 
-        assumes \forall int i; 0 <= i < l-p-1; x[i] == x[i+p];
-        ensures \result = 1;
+      assumes \forall int i; 0 <= i < l-p-1 ==> x[i] == x[i+p];
+      ensures \result == 1;
 
     behavior two:
-        assumes !(\forall int i; 0 <= i < l-p-1; x[i] == x[i+p]);
-        ensures \result = 0;
+      assumes !(\exists int i; 0 <= i < l-p-1 ==> x[i] == x[i+p]);
+      ensures \result == 0;
 
     complete behaviors zero,one,two;
-    disjoint behaviors zero,one,two;
+    disjoint behavior zero,one,two;
 */
 
-bool has_period(char x[], int p, unsigned l) {
-    if (p == l) return true;
-    if ((l % p) != 0) return false;
+unsigned has_period(const char x[], int p, unsigned l) {
+    if (p == l) return 1;
+    if ((l % p) != 0) return 0;
+
+    int i = 0;
 
         /*@
             loop assigns i;
 
-            loop invariant (\forall int i; 0 <= i <= l-p-1; )
+            loop invariant \forall int i; 0 <= i < l-p-1 ==> p == 1;
         */
         for (int i = 0 ; i < l-p-1 ; ++i) {
             if (x[i] != x[i + p])
-                return false;
+                return 0;
         }
 
-    return true;
+    return 1;
 }
 
 
 /*@
-   predicate has_period(char[] x, int p, unsigned l) =
-      \forall int i; i <= l-p-1 ==>  x[i] == x[i+p]
+   predicate has_period(char* x, unsigned int p, unsigned l) =
+      \forall int i; i <= (l-p-1) ==>  x[i] == x[i+p];
 */
 
 /*@
-    requires l >= 0;
+    requires l > 0;
     requires \valid(x+(0..l-1));
     
-    assigns \nothing;
-
+    //ensures \forall int i; 0 < i <= l ==> !(has_period(x,\result,l));
     ensures 0 < \result <= l;
-    ensures \forall int i; 0 < i < p ==> !(has_period(x,p,l));
-    ensures has_period(x,p,l);
 */
 
-unsigned per(char x[], unsigned l) {
-    /*@
-        loop assigns p;
+unsigned per(const char x[], unsigned l) {
+     int p = 1;
 
-        loop invariant 1 <= p <= l;
-        loop invariant 0 <= p <= l;
-        loop variant l - p;
+    /*@
+        loop invariant 0 < p <= l;
     */
-    for(int p = 1; p <= l; ++p) {
-        if(has_period(x,p,l))
-            return p;
-    }
+
+    while(p <= l && !has_period(x,p,l)) 
+        ++p;
+
+    return 1;
 }
